c2b013d0274a2ae5c0b9ac328999c4fd35d56688
Karol Żelazny <hello@kzelazny.pl>
2026-02-16 22:28:01 +0100

    First commit

---
+++ modules/dashproducts/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/dashproducts',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cfb0cb933fcc2b0315a31d96060cb21f32fd7014',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/dashproducts' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cfb0cb933fcc2b0315a31d96060cb21f32fd7014',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
